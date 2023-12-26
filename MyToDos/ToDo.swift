dbbe06c2840a3ef0597f0b3bdcedb69e26ea9799
Atikur Rahman <mratikurrahman@gmail.com>
2023-12-26 23:25:36 +0600

    Created ToDo model struct

--- MyToDos/ToDo.swift
+++ MyToDos/ToDo.swift
@@ -7,4 +7,16 @@
 
 import Foundation
 
-
+struct ToDo: Identifiable, Codable {
+    var id: String = UUID().uuidString
+    var name: String
+    var completed: Bool = false
+    
+    static var sampleData: [ToDo] {
+        [
+            ToDo(name: "Get Lunches"),
+            ToDo(name: "Meeting at afternoon"),
+            ToDo(name: "Dinner with family friends")
+        ]
+    }
+}

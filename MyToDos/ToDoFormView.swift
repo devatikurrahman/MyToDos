bf08699ea99c3b2bed51b70df9d00d81218be77e
Atikur Rahman <mratikurrahman@gmail.com>
2023-12-29 01:13:29 +0600

    Added ToDoFormView

---
+++ MyToDos/ToDoFormView.swift
@@ -0,0 +1,59 @@
+//
+//  ToDoFormView.swift
+//  MyToDos
+//
+//  Created by Atikur Rahman on 12/29/23.
+//
+
+import SwiftUI
+
+struct ToDoFormView: View {
+    @EnvironmentObject var dataStore: DataStore
+    @ObservedObject var formVM: ToDoFormViewModel
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                VStack(alignment: .leading) {
+                    TextField("ToDo", text: $formVM.name)
+                    Toggle("Completed", isOn: $formVM.completed)
+                }
+            }
+        }
+        .navigationTitle("ToDo")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarItems(leading: cancelButton, trailing: updateSaveButton)
+    }
+}
+
+extension ToDoFormView {
+    func updateToDo() {
+        let toDo = ToDo(id: formVM.id!, name: formVM.name, completed: formVM.completed)
+        dataStore.updateToDo(toDo)
+        presentationMode.wrappedValue.dismiss()
+    }
+    
+    func addToDo() {
+        let toDo = ToDo(id: formVM.id!, name: formVM.name, completed: formVM.completed)
+        dataStore.addToDo(toDo)
+        presentationMode.wrappedValue.dismiss()
+    }
+    
+    var cancelButton: some View {
+        Button("Cancel") {
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    var updateSaveButton: some View {
+        Button(formVM.updating ? "Update" : "Save",
+               action: formVM.updating ? updateToDo : addToDo)
+        .disabled(formVM.updating)
+    }
+}
+
+#Preview {
+    ToDoFormView(formVM: ToDoFormViewModel())
+        .environmentObject(DataStore())
+}

b9a1f3045012d1a9898b84156f0aa5a7bd9181f6
Atikur Rahman <mratikurrahman@gmail.com>
2023-12-29 00:47:51 +0600

    Created ToDoFromViewModel class

---
+++ MyToDos/ToDoFormViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoFormViewModel.swift
+//  MyToDos
+//
+//  Created by Atikur Rahman on 12/29/23.
+//
+
+import Foundation
+
+final class ToDoFormViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var completed = false
+    var id: String?
+    
+    var updating: Bool {
+        id != nil
+    }
+    
+    var isDisabled: Bool {
+        name.isEmpty
+    }
+    
+    init() {}
+    
+    init(_ currentToDo: ToDo) {
+        self.name = currentToDo.name
+        self.completed = currentToDo.completed
+        id = currentToDo.id
+    }
+}

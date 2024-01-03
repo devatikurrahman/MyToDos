8c44da00ee1ac466825d913e5506f54db558f8ce
Atikur Rahman <mratikurrahman@gmail.com>
2024-01-03 23:34:20 +0600

    Created ModalType enum

---
+++ MyToDos/ModalType.swift
@@ -0,0 +1,32 @@
+//
+//  ModalType.swift
+//  MyToDos
+//
+//  Created by Atikur Rahman on 1/3/24.
+//
+
+import SwiftUI
+
+enum ModalType: Identifiable, View {
+    case new
+    case update(ToDo)
+    var id: String {
+        switch self {
+        case .new:
+            return "new"
+        case .update:
+            return "update"
+        }
+    }
+    
+    var body: some View {
+        switch self {
+        case .new:
+            return ToDoFormView(formVM: ToDoFormViewModel())
+        case .update(let toDo):
+            return ToDoFormView(formVM: ToDoFormViewModel(toDo))
+        }
+    }
+}
+
+

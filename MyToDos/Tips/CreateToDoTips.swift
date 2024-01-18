6e3ab71607ed5bf6977ee2e081b55466638821b3
Atikur Rahman <mratikurrahman@gmail.com>
2024-01-18 23:54:00 +0600

    Create CreateToDoTips struct

---
+++ MyToDos/Tips/CreateToDoTips.swift
@@ -0,0 +1,17 @@
+//
+//  CreateToDoTips.swift
+//  MyToDos
+//
+//  Created by Atikur Rahman on 1/18/24.
+//
+
+import Foundation
+import TipKit
+
+struct CreateToDoTips: Tip {
+    var title: Text {
+        Text("Create your first ToDo")
+    }
+    
+    
+}

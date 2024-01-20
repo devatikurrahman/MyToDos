841fa5e1e61d5fff4e2bcc19ca0d46fe90b79c45
Atikur Rahman <mratikurrahman@gmail.com>
2024-01-20 17:18:32 +0600

    Updated saving and loading ToDos data from file with Combine framework

--- MyToDos/ViewModels/DataStore.swift
+++ MyToDos/ViewModels/DataStore.swift
@@ -15,37 +15,97 @@ final class DataStore: ObservableObject {
     var addToDo = PassthroughSubject<ToDo, Never>()
     var updateToDo = PassthroughSubject<ToDo, Never>()
     var deleteToDo = PassthroughSubject<IndexSet, Never>()
+    var loadToDo = Just(FileManager.docDirURL.appendingPathComponent(fileName))
     
     var subscriptions = Set<AnyCancellable>()
     
     init() {
         print(FileManager.docDirURL.path)
         addSubscriptions()
-        if FileManager().docExist(name: fileName) {
+        /*
+         if FileManager().docExist(name: fileName) {
             loadToDo()
-        }
+         }
+         */
     }
     
     func addSubscriptions() {
-        addToDo.sink { [unowned self] toDo in
-            toDoList.append(toDo)
-            saveToDos()
-        }
-        .store(in: &subscriptions)
+        loadToDo
+            .filter { FileManager.default.fileExists(atPath: $0.path) }
+            .tryMap { url in
+                try Data(contentsOf: url)
+            }
+            .decode(type: [ToDo].self, decoder: JSONDecoder())
+            .subscribe(on: DispatchQueue(label: "background queue"))
+            .receive(on: DispatchQueue.main)
+            .sink { [unowned self] completion in
+                switch completion {
+                case .finished:
+                    print("Loading Completed")
+                    toDosSubscription()
+                case .failure(let error):
+                    if error is ToDoError {
+                        appError = ErrorType(error: error as! ToDoError)
+                    } else {
+                        appError = ErrorType(error: ToDoError.decodingError)
+                        toDosSubscription()
+                    }
+                }
+            } receiveValue: { (toDos) in
+                self.toDoList = toDos
+            }
+            .store(in: &subscriptions)
+        
         
         updateToDo.sink { [unowned self] toDo in
             guard let index = toDoList.firstIndex(where: { $0.id == toDo.id}) else { return }
             toDoList[index] = toDo
-            saveToDos()
+            //saveToDos()
         }
         .store(in: &subscriptions)
         
         deleteToDo.sink { [unowned self] indexSet in
             toDoList.remove(atOffsets: indexSet)
-            saveToDos()
+            //saveToDos()
         }
         .store(in: &subscriptions)
     }
+    
+    
+    func toDosSubscription() {
+        $toDoList
+            .subscribe(on: DispatchQueue(label: "background queue"))
+            .receive(on: DispatchQueue.main)
+            .dropFirst()
+            .encode(encoder: JSONEncoder())
+            .tryMap { data in
+                try data.write(to: FileManager.docDirURL.appendingPathComponent(fileName))
+            }
+            .sink { [unowned self] completion in
+                switch completion {
+                case .finished:
+                    print("Saving Completed")
+                case .failure(let error):
+                    if error is ToDoError {
+                        appError = ErrorType(error: error as! ToDoError)
+                    } else {
+                        appError = ErrorType(error: ToDoError.encodingError)
+                    }
+                }
+            } receiveValue: { _ in
+                print("Saving file was successful")
+            }
+            .store(in: &subscriptions)
+
+        
+        
+        addToDo.sink { [unowned self] toDo in
+            toDoList.append(toDo)
+            //saveToDos()
+        }
+        .store(in: &subscriptions)
+    }
+    
     
     /*
     func addToDo(_ toDo: ToDo) {
@@ -64,6 +124,7 @@ final class DataStore: ObservableObject {
     }
      */
     
+    /*
     func loadToDo() {
         //toDoList = ToDo.sampleData
         FileManager().readDocument(docName: fileName) { (result) in
@@ -83,7 +144,9 @@ final class DataStore: ObservableObject {
             
         }
     }
+    */
     
+    /*
     func saveToDos() {
         print("Saving toDos data")
         let encode = JSONEncoder()
@@ -101,4 +164,5 @@ final class DataStore: ObservableObject {
             appError = ErrorType(error: .encodingError)
         }
     }
+     */
 }
